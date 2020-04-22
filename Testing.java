0e77b2b627e1700092b240c47a86930039eba0e8
SergioDAM1 <sergiinside07@gmail.com>
2020-04-22 12:13:37 +0200

    Creacion del archivo Texting.java

---
+++ Testing.java
@@ -0,0 +1,18 @@
+package PROSergio;
+
+public class Testing {
+
+	Testing(){
+		
+	}
+	
+	void Visualizar() {
+		System.out.println("Bienvenido a GitHub.");
+	}
+	
+	public static void main(String[] args) {
+		Testing prueba = new Testing();
+		prueba.Visualizar();
+	}
+
+}
