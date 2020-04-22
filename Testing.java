bd42931d16adadfbaf62ee206e0e109be02fe1ae
SergioDAM1 <Sergiinside07@gmail.com>
2020-04-22 13:18:45 +0200

    Intento de generar errores

--- Testing.java
+++ Testing.java
@@ -9,6 +9,7 @@ public class Testing {
 	void Visualizar() {
 		System.out.println("Bienvenido a GitHub.");
 		System.out.println("Cambio del usuario 2.");
+		System.out.println("Prueba subidas de 2 usuarios.");
 	}
 	
 	public static void main(String[] args) {
